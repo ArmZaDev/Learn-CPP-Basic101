83ffb9689da0b4cb7994c1f9255ad7cb3d10464c
ArmZaDev <arztv001@gmiail.com>
2022-07-19 15:29:43 +0700

    Variables and basic data types

--- MyProgram01/MyProgram01.cpp
+++ MyProgram01/MyProgram01.cpp
@@ -3,9 +3,35 @@ using namespace std;
 
 int main()
 {
-    cout << "peaky fookin blinders\n";
-    cout << "peaky " << "fookin" << "blinders" << endl;
-    cout << "peaky\tfookin\tblinders";
+    // ประกาศตัวแปร
+    // ชนิดข้อมูล ชื่อตัวแปร
+
+    int number1;
+    int number2 = 200;
+
+    // int number1 = 100, number2 = 200;
+
+    number1 = 100;
+    number2 = 26;
+
+    string name = "tommy";
+    float score = 80.50;
+    int age = 30;
+    bool pass = true;
+    char grade = 'B';
+
+    // Output
     
+    //cout << "Number1 = " << number1 << endl;
+    //cout << "Number2 = " << number2 << "\n";
+
+    //cout << "Score = " << score << "\n";
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Score: " << score << endl;
+    cout << "Pass: " << pass << endl;
+    cout << "Grade: " << grade << "\n";
+
 }
 
