5a1ae9f915564c076faf8c867b4d904ed8c44d3b
ArmZaDev <arztv001@gmail.com>
2022-07-19 15:40:19 +0700

    Constant

--- MyProgram01/MyProgram01.cpp
+++ MyProgram01/MyProgram01.cpp
@@ -3,35 +3,9 @@ using namespace std;
 
 int main()
 {
-    // ประกาศตัวแปร
-    // ชนิดข้อมูล ชื่อตัวแปร
+    const float PI = 3.1415;
 
-    int number1;
-    int number2 = 200;
-
-    // int number1 = 100, number2 = 200;
-
-    number1 = 100;
-    number2 = 26;
-
-    string name = "tommy";
-    float score = 80.50;
-    int age = 30;
-    bool pass = true;
-    char grade = 'B';
-
-    // Output
-    
-    //cout << "Number1 = " << number1 << endl;
-    //cout << "Number2 = " << number2 << "\n";
-
-    //cout << "Score = " << score << "\n";
-
-    cout << "Name: " << name << endl;
-    cout << "Age: " << age << endl;
-    cout << "Score: " << score << endl;
-    cout << "Pass: " << pass << endl;
-    cout << "Grade: " << grade << "\n";
+    cout << "PI = " << PI;
 
 }
 
