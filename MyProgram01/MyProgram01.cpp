1221f0ac6f7e3c4989cb353996f8c09f14179a06
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-20 15:23:38 +0700

    Nested if

--- MyProgram01/MyProgram01.cpp
+++ MyProgram01/MyProgram01.cpp
@@ -3,54 +3,32 @@ using namespace std;
 
 int main()
 {
-    string name;
-    string status;
-    int score;
-    char grade;
-    //input    
+    int amount = 0;
+    cout << "Input your amount: ";
+    cin >> amount;
+    cout << "Amount = " << amount << "\n";
 
-    cout << "Input student name: ";
-    cin >> name;
-    cout << "Input score (0-100): ";
-    cin >> score;
-
-    //output
-    cout << "-----Description-----" << "\n";
-    cout << "Student: " << name << "\n";
-    cout << "Score: " << score << "\n";
-
-    if (score >= 80 && score >= 100)
+    if (amount >= 15000)
     {
-        grade = 'A';
-        cout << "Grade = " << grade << "\n";
-    }
-    else if (score >= 70 && score <= 79)
-    {
-        grade = 'B';
-        cout << "Grade = " << grade << "\n";
-    }
-    else if (score >= 60 && score <= 69)
-    {
-        grade = 'C';
-        cout << "Grade = " << grade << "\n";
-    }
-    else if (score >= 50 && score <= 59)
-    {
-        grade = 'D';
-        cout << "Grade = " << grade << "\n";
-    }
-    else if(score >=0 && score <= 49)
-    {
-        grade = 'F';
-        cout << "Grade = " << grade << "\n";
+        cout << "You have discount";
+        if(amount == 15000)
+        {
+            cout << " 10%" << "\n";
+        }
+        else if (amount == 20000)
+        {
+            cout << " 20%" << "\n";
+        }
+        else 
+        {
+            cout << " 50%" << "\n";
+        }
+        
     }
     else
     {
-        grade = 'N';
-        cout << "Grade = " << grade << "\n";
-        cout << "Input Error! Try again." << "\n";
+        cout << "You don't have discount";
     }
-    
     
 }
 
