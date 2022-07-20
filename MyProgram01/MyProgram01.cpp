ceb0dba5d2c852364fba7733640b8a7c93b9a593
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-20 17:10:10 +0700

    Summation

--- MyProgram01/MyProgram01.cpp
+++ MyProgram01/MyProgram01.cpp
@@ -3,21 +3,28 @@ using namespace std;
 
 int main()
 {
-    for (int i = 1; i <= 5; i++) {
-        cout << "Peaky fookin blinders" << "\n";
-    }
-
-    /*
     int number = 0;
-
-    cout << "Input number: ";
-    cin >> number;
-
-    for (int i = 1; i <= 12; i++) {
-        cout << number << " X " << i << " = " << number * i << "\n";
+    int sum = 0;
+    /*
+    for (int count = 1; count <= 5; count++) {
+        cout << "Input your number: ";
+        cin >> number;
+        sum += number;
     }
-    cout << "End Program" << endl;
+    cout << "Summation = " << sum;
     */
+
+
+    int count = 1;
+    do {
+        cout << "Input your number: ";
+        cin >> number;
+        sum += number;
+        count++;
+    } while (count <= 2);
+    cout << "Summation = " << sum;
+
+    
     
 }
 
